8a24813a15c4fc69aab1f278c2033760810fed1d
Jennifer Ng <jenchy8@gmail.com>
2020-12-29 22:48:25 -0500

    Minor changes

--- src/main/java/com/devsight/templatespringboot/featurename/controller/TemplateController.java
+++ src/main/java/com/devsight/templatespringboot/featurename/controller/TemplateController.java
@@ -4,9 +4,11 @@ import com.devsight.templatespringboot.featurename.model.TemplateEntity;
 import com.devsight.templatespringboot.featurename.service.TemplateService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/feature")
 public class TemplateController {
 
     @Autowired
